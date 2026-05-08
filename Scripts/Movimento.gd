2b9e1101a8f9c9d37b0b04a637a409c7398acbd5
MyR4YT <Ssonicmc@gmail.com>
2026-05-08 12:15:03 -0300

    fhbebdchefc
    ahhhhhhhhhhhhhhhhhhh

--- Scripts/Movimento.gd
+++ Scripts/Movimento.gd
@@ -5,31 +5,25 @@ var JUMP_VELOCITY = -400.0
 var is_hurting := false 
 
 @onready var anim = $AnimatedSprite2D
+@onready var anit = $AnimationPlayer
+var side = 2
 
 func _physics_process(delta: float) -> void:
-	# 1. Aplica a gravidade sempre
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# 2. Lógica do Knockback
 	if is_hurting:
-		# A MÁGICA AQUI: Só sai do estado de dano se tocar o chão E a velocidade Y
-		# for maior ou igual a zero (ou seja, você já subiu e agora caiu/pousou).
 		if is_on_floor() and velocity.y >= 0:
 			is_hurting = false
 		else:
-			# Se ainda está voando pelo empurrão, apenas move e encerra o frame
 			move_and_slide()
-			return 
-
-	# --- DAQUI PRA BAIXO É O MOVIMENTO NORMAL (só roda se is_hurting for false) ---
+			return
 
 	if Input.is_action_just_pressed("pulo") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("left", "right")
 	
-	# Gerenciamento de Animações
 	if not is_on_floor():
 		if velocity.y < 0:
 			anim.play("Jump")
@@ -37,11 +31,17 @@ func _physics_process(delta: float) -> void:
 			anim.play("Fall")
 	elif direction != 0:
 		anim.play("Walk")
-		anim.flip_h = (direction < 0)
+		if direction > 0 and side != 1:
+			print("b")
+			side = 1
+			anit.play("flip")
+		elif direction < 0 and side != 2:
+			print("a")
+			side = 2
+			anit.play("flip2")
 	else:
 		anim.play("Idle")
 
-	# Movimentação Horizontal
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -49,27 +49,25 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-# --- FUNÇÕES EXTRAS ---
-
 func take_damage(knockback_force := Vector2(-200, -200)):
 	if is_hurting: return 
 	
 	is_hurting = true
 	Global.health -= 1
 	
-	# Aplica o empurrão (Isso faz a velocity.y ficar negativa instantaneamente)
 	velocity = knockback_force
 	anim.play("Damage")
 	
 	if Global.health <= 0:
 		die()
 	
-	# Removemos totalmente o Timer daqui! 
-	# A física no _physics_process agora cuida de saber a hora exata de parar.
-
-# No arquivo Player.gd
 func die():
 	Global.health = 3
 	Global.coins = 0
 	# Em vez de reload_current_scene(), chamamos a tela nova!
 	get_tree().change_scene_to_file("res://Scenes/game_over.tscn")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("d")
+	die()
